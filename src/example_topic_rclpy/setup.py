13571eadaa74649cf7f48a7c22298b35a8edaecc
rocketman123456 <759094438@qq.com>
2023-09-01 14:56:23 +0800

    add python topic example

--- src/example_topic_rclpy/setup.py
+++ src/example_topic_rclpy/setup.py
@@ -20,6 +20,8 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
+            "topic_publisher_02 = example_topic_rclpy.topic_publisher_02:main",
+            "topic_subscribe_02 = example_topic_rclpy.topic_subscribe_02:main"
         ],
     },
 )
