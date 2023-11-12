021bb9d5743864836a005a887e7f42718fa183c2
rocketman123456 <759094438@qq.com>
2023-11-12 15:16:43 +0800

    add first plugin

---
+++ src/example_parameters_py/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+
+package_name = 'example_parameters_py'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=[package_name],
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='rocketsky',
+    maintainer_email='759094438@qq.com',
+    description='Python parameter tutorial',
+    license='Apache License 2.0',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'minimal_param_node = example_parameters_py.python_parameters_node:main',
+        ],
+    },
+)
