9f772a704e7b6fd4e11d56d8b07bc5b4fe20a377
rocketman123456 <759094438@qq.com>
2023-12-05 18:58:07 +0800

    update

--- src/example/example_action_py/setup.py
+++ src/example/example_action_py/setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup
 
-package_name = 'action_tutorials_py'
+package_name = 'example_action_py'
 
 setup(
     name=package_name,
@@ -20,8 +20,8 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-            "fibonacci_action_server = action_tutorials_py.fibonacci_action_server:main",
-            "fibonacci_action_client = action_tutorials_py.fibonacci_action_client:main",
+            "fibonacci_action_server = example_action_py.fibonacci_action_server:main",
+            "fibonacci_action_client = example_action_py.fibonacci_action_client:main",
         ],
     },
 )
